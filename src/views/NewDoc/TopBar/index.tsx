3f40cc336b7d69f852399bf9cf36db7689a8213e
N07Cc <2545838878@qq.com>
2024-07-12 10:19:52 +0800

    feat: 移除TopBar中title相关props

--- src/views/NewDoc/TopBar/index.tsx
+++ src/views/NewDoc/TopBar/index.tsx
@@ -6,16 +6,12 @@ import ActionButton from "./ActionButton";
 import LoginButton from "./LoginButton";
 import DropdownMenu from "./DropdownMenu";
 
-interface TopBarProps {
-  title: string;
-  setTitle: (title: string) => void;
-}
-const TopBar: React.FC<TopBarProps> = ({ title, setTitle }) => {
+const TopBar = () => {
   return (
     <div className="w-full flex items-center justify-between bg-white p-4 shadow-md border-b border-gray-300 h-14">
       <div className="flex items-center space-x-4">
         <ExitButton />
-        <TopBarTitle title={title} setTitle={setTitle} />
+        <TopBarTitle />
         {/* <ActionButton label="开始" />
         <ActionButton label="效率" />
         <ActionButton label="审阅" /> */}
