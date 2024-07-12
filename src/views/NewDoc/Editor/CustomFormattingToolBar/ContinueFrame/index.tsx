75008dcab397c7aafeb69e5b6e3d4d5c2476ebe4
cl <long.chen@transwarp.cn>
2024-07-13 01:51:57 +0800

    fix: 续写文字的插入

--- src/views/NewDoc/Editor/CustomFormattingToolBar/ContinueFrame/index.tsx
+++ src/views/NewDoc/Editor/CustomFormattingToolBar/ContinueFrame/index.tsx
@@ -1,7 +1,7 @@
 import TextArea, { TextAreaRef } from "antd/es/input/TextArea";
 import { useNewDocState } from "../../../utils/provider";
 import React, { useEffect, useRef, useCallback } from "react";
-import { StyleSchema, StyledText } from "@blocknote/core";
+import { PartialBlock, StyleSchema, StyledText } from "@blocknote/core";
 import { Button, Space } from "antd";
 
 const ContinueFrame = () => {
@@ -25,24 +25,61 @@ const ContinueFrame = () => {
   }, [continText]);
   // 点击插入后
   const handleInsert = useCallback(() => {
-    const originBlock = editor!.getBlock(blockToUpdate!);
-    const originContentArr = originBlock!.content as Array<
-      StyledText<StyleSchema>
-    >;
-    if (originBlock?.type == "paragraph") {
-      const newContentArr = originContentArr.map((item) => {
-        const newText =
-          item.text.substring(0, range.startOffset) +
-          continText +
-          item.text.substring(range.endOffset);
-        return { ...item, text: newText };
-      });
-      editor?.updateBlock(blockToUpdate!, {
-        content: newContentArr,
-      });
-    } else {
-      return;
-    }
+    const selection = editor?.getSelection();
+    const lastBlock = selection?.blocks[selection?.blocks.length - 1];
+    const blocksToInsert: Array<PartialBlock> = new Array<PartialBlock>();
+    continText.split("\n\n").map((substr, index) => {
+      console.log(index, substr);
+      if (index == 0) {
+        const newLastBlockText =
+          (lastBlock?.content as StyledText<StyleSchema>[])[0].text.substring(
+            0,
+            range.endOffset
+          ) + substr;
+        editor?.updateBlock(lastBlock!, {
+          content: [
+            {
+              ...(lastBlock?.content as StyledText<StyleSchema>[])[0],
+              text: newLastBlockText,
+            },
+          ],
+        });
+      } else {
+        blocksToInsert.push({
+          type: "paragraph",
+          content: "",
+        });
+        blocksToInsert.push({
+          type: "paragraph",
+          content: substr,
+        });
+      }
+    });
+    blocksToInsert.push({
+      type: "paragraph",
+      content: (
+        lastBlock?.content as StyledText<StyleSchema>[]
+      )[0].text.substring(range.endOffset),
+    });
+    editor?.insertBlocks(blocksToInsert, lastBlock!, "after");
+    // const originBlock = editor!.getBlock(blockToUpdate!);
+    // const originContentArr = originBlock!.content as Array<
+    //   StyledText<StyleSchema>
+    // >;
+    // if (originBlock?.type == "paragraph") {
+    //   const newContentArr = originContentArr.map((item) => {
+    //     const newText =
+    //       item.text.substring(0, range.startOffset) +
+    //       continText +
+    //       item.text.substring(range.endOffset);
+    //     return { ...item, text: newText };
+    //   });
+    //   editor?.updateBlock(blockToUpdate!, {
+    //     content: newContentArr,
+    //   });
+    // } else {
+    //   return;
+    // }
   }, [
     editor,
     blockToUpdate,
@@ -53,7 +90,7 @@ const ContinueFrame = () => {
   ]);
   return (
     <div
-      className={`${continButtnDisplay} border-x border-b border-gray-100 rounded-e-md shadow-lg pt-5 pb-2.5 pl-2.5 pr-7`}
+      className={`${continButtnDisplay} bg-white border-x border-b border-gray-100 rounded-e-md shadow-lg pt-5 pb-2.5 pl-2.5 pr-7`}
     >
       <TextArea
         autoSize={{ minRows: 1, maxRows: 5 }}
