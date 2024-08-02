99a6ccb7cb4d91da93915a820fb2ffd1bb4c67e3
N07Cc <2545838878@qq.com>
2024-08-02 10:39:16 +0800

    fix: 为POST请求添加X-Authorization Header

--- src/views/Home/Knowledge/api/index.ts
+++ src/views/Home/Knowledge/api/index.ts
@@ -18,9 +18,8 @@ export class KnowledgeApi {
   public async createKnowledge(body: CreateKnowledgeParams, token: string) {
     const result = await axios.post(
       `${apikeyPrefix}/db/new?db_name=${body.db_name}`,
-      {
-        headers: { "X-Authorization": `Bearer ${token}` },
-      }
+      {},
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data;
   }
@@ -28,9 +27,8 @@ export class KnowledgeApi {
   public async delKnowledge(db_name: string, token: string) {
     const result = await axios.post(
       `${apikeyPrefix}/db/delete?db_name=${db_name}`,
-      {
-        headers: { "X-Authorization": `Bearer ${token}` },
-      }
+      {},
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data;
   }
@@ -38,9 +36,7 @@ export class KnowledgeApi {
   public async downloadKnowledge(db_name: string, token: string) {
     const result = await axios.get(
       `${apikeyPrefix}/db/download?db_name=${db_name}`,
-      {
-        headers: { "X-Authorization": `Bearer ${token}` },
-      }
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data;
   }
@@ -48,9 +44,7 @@ export class KnowledgeApi {
   public async getFiles(db_name: string, token: string): Promise<FileBase[]> {
     const result = await axios.get(
       `${apikeyPrefix}/file/list?db_name=${db_name}`,
-      {
-        headers: { "X-Authorization": `Bearer ${token}` },
-      }
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data.data.file_list;
   }
@@ -58,9 +52,8 @@ export class KnowledgeApi {
   public async delFile(db_name: string, file_name: string, token: string) {
     const result = await axios.post(
       `${apikeyPrefix}/file/delete?db_name=${db_name}&file_name=${file_name}`,
-      {
-        headers: { "X-Authorization": `Bearer ${token}` },
-      }
+      {},
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data;
   }
@@ -68,9 +61,7 @@ export class KnowledgeApi {
   public async downloadFile(db_name: string, file_name: string, token: string) {
     const result = await axios.get(
       `${apikeyPrefix}/file/download?db_name=${db_name}&file_name=${file_name}`,
-      {
-        headers: { "X-Authorization": `Bearer ${token}` },
-      }
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data;
   }
@@ -78,7 +69,8 @@ export class KnowledgeApi {
   public async uploadFile(db_name: string, knowledge_files: [], token: string) {
     const result = await axios.post(
       `${apikeyPrefix}/file/upload?db_name=${db_name}`,
-      { knowledge_files, headers: { "X-Authorization": `Bearer ${token}` } }
+      { knowledge_files },
+      { headers: { "X-Authorization": `Bearer ${token}` } }
     );
     return result.data;
   }
