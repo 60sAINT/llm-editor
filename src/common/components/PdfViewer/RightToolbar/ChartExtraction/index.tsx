9969583b939404203af6da7cacf815a07142c1ae
60sAINT <2950712207@qq.com>
2024-08-11 15:00:27 +0800

    fix: 删除无用引用

--- src/common/components/PdfViewer/RightToolbar/ChartExtraction/index.tsx
+++ src/common/components/PdfViewer/RightToolbar/ChartExtraction/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import wenxin from "@/assets/wenxin.png";
 import {
   HighlightPlugin,
